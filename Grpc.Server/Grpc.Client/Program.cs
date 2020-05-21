1037d6c46aee2482d94872aa6d9098bb6ffb570a
Sureshkumar Duraisamy <sureshkumar.duraisamy@gmail.com>
2020-05-21 14:42:09 +0000

    Base Sample Code

---
+++ Grpc.Server/Grpc.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using Grpc.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //creating the channel with GRPC server URL
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //Creating the Client Instance of Greeter Server using the channel
+            var client = new Greeter.GreeterClient(channel);
+            //Consuming the Grpc Server Service using RPC call from Grpc Client 
+            HelloReply respone = await client.SayHelloAsync(new HelloRequest { Name = "SafeAuto" });
+
+            Console.WriteLine($"{respone.Message}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Grpc.Server/Grpc.Server/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Grpc.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+
+
+            //services.AddSwaggerGen(options =>
+            //{
+            //    options.SwaggerDoc("v1",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo
+            //        {
+            //            Title ="Swagger Demo API",
+            //            Description = "Demo API for showing Swagger",
+            //            Version="v1"
+                       
+            //        });
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+
+            //app.UseSwagger();
+            //app.UseSwaggerUI(options => {
+
+            //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo API");
+            //});
+        }
+    }
+}
